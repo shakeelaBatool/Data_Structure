7e1a3b78b497f8e8920356f112be9da505b5018d
Shakeela Batool <shakeelasadpara@gmail.com>
2025-12-29 16:24:38 +0500

    Add files via upload

---
+++ element_inlinked_list.py
@@ -0,0 +1,15 @@
+# Print element of the linked list
+
+class Node:
+    def __init__(self, data):
+        self.data=data
+        self.next=None
+n1=Node(12)
+n1.next=Node(13)
+n1.next.next=Node(14)
+n1.next.next.next=Node(15)
+
+while n1 != None:
+    print(n1.data,"➡  ", end="")
+    n1=n1.next
+print("None")
