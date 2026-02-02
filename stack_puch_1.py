b3379d122231d4f65a4fadf3a1f627588a5a88cb
Shakeela Batool <shakeelasadpara@gmail.com>
2026-02-02 20:58:40 +0500

    Add files via upload

---
+++ stack_puch_1.py
@@ -0,0 +1,16 @@
+stack=[]
+def push(x):
+    stack.append(x)
+def pop():
+    if len(stack)==0:
+        print("Stack is empty")
+    else:
+        print(stack.pop())
+
+push(9)
+push(11)
+push(12)
+pop()
+pop()
+pop()
+pop()
