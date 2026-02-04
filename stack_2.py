64b3683bbeb614514014ed58fe43643144744563
Shakeela Batool <shakeelasadpara@gmail.com>
2026-02-04 21:05:39 +0500

    Add files via upload

---
+++ stack_2.py
@@ -0,0 +1,7 @@
+stack=[]                               # Last In, First Out
+stack.append("palte1")
+stack.append("palte2")
+stack.append("palte3")
+stack.append("palte4")
+print(stack.pop(),stack.pop())
+print(stack[0])
