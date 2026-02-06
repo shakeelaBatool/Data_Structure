a9a1aa9001ad0209f5e785f7de78fe5117638e91
Shakeela Batool <shakeelasadpara@gmail.com>
2026-02-06 18:15:39 +0500

    Update stack_2.py

--- stack_2.py
+++ stack_2.py
@@ -1,7 +1,8 @@
-stack=[]                               # Last In, First Out
+stack= []                               # Last In, First Out
 stack.append("palte1")
 stack.append("palte2")
 stack.append("palte3")
 stack.append("palte4")
 print(stack.pop(),stack.pop())
+
 print(stack[0])
