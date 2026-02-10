3cb931f58a9b3cbd2af67d2c0083224cd72d2b4f
Shakeela Batool <shakeelasadpara@gmail.com>
2026-02-10 21:48:26 +0500

    Update stack_puch_1.py

--- stack_puch_1.py
+++ stack_puch_1.py
@@ -2,7 +2,7 @@ stack=[]
 def push(x):
     stack.append(x)
 def pop():
-    if len(stack)==0:
+    if len(stack)== 0:
         print("Stack is empty")
     else:
         print(stack.pop())
@@ -16,3 +16,4 @@ pop()
 pop()
 
 pop()
+
