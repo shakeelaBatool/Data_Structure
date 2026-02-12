35503271e198345a57256bfa63c1be03e98ac4ce
Shakeela Batool <shakeelasadpara@gmail.com>
2026-02-12 12:59:28 +0500

    Update search_ele.py

--- search_ele.py
+++ search_ele.py
@@ -2,7 +2,7 @@ class Node:
     def __init__(self, data):
         self.data=data
         self.next=None    
-n1=Node(13)
+n1= Node(13)
 n1.next= Node(14)
 n1.next.next=Node(15)
 n1.next.next.next= Node(16)
@@ -15,6 +15,7 @@ while temp != None:
         print("Find ")
         break   
     temp=temp.next
+
 
 
 
