b3e7db5f8de2c64355cdecf2d193d6647a5cbcf0
Shakeela Batool <shakeelasadpara@gmail.com>
2026-02-12 12:58:52 +0500

    Add files via upload

---
+++ insert_node_at_end.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, data):
+        self.data=data
+        self.next=None
+
+def insert_end(head, data):
+    new=Node(data)
+    if head is None:
+        return new
+    temp=head
+    while temp.next is not None:
+        temp=temp.next
+    temp.next=new
+    return head
+def display(head):
+    temp=head
+    while temp is not None:
+        print(temp.data,end="-->")
+        temp=temp.next
+    print(None)
+
+n=int(input("Enter the number oof node: "))
+head=None
+for i in range(n):
+    val=int(input("Enter the element: "))
+    head=insert_end(head, val)
+display(head)
