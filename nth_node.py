0087276b0b2a6917bf31433b6ed6e2d9fb212e32
Shakeela Batool <shakeelasadpara@gmail.com>
2026-02-20 21:25:29 +0500

    Update nth_node.py

--- nth_node.py
+++ nth_node.py
@@ -6,7 +6,7 @@ def display(head):
     temp = head
     while temp:
         print(temp.data, end="-->")
-        temp = temp.next
+        temp =temp.next
     print("None")
 def re_node(head, n):
     if n == 1:
@@ -30,6 +30,7 @@ n = int(input("Which node to remove: "))
 head = re_node(head, n)
 print("Updated List:")
 display(head)
+
 
 
 
