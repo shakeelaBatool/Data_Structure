7b44e2fc9a8e27f54955084cbff068312e4ada94
Shakeela Batool <shakeelasadpara@gmail.com>
2026-02-20 21:25:13 +0500

    Update len_linked_list.py

--- len_linked_list.py
+++ len_linked_list.py
@@ -4,7 +4,7 @@ class Node:
         self.next= None
        
 n1= Node(12)
-n2=Node(13)
+n2= Node(13)
 n3=Node(14)
 
 n1.next= n2
@@ -14,6 +14,7 @@ while n1!=None:
     count+= 1
     n1=n1.next
 print(f'Linght of Node is {count}')
+
 
 
 
