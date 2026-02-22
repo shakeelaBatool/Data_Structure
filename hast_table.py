e52f74458ef0399a5a689bf969be964cf27aeae4
Shakeela Batool <shakeelasadpara@gmail.com>
2026-02-22 13:06:16 +0500

    Add files via upload

---
+++ hast_table.py
@@ -0,0 +1,10 @@
+table=[None]*10
+print(len(table))
+name_1="Ali"
+value=25
+index=0
+for i in name_1:
+    index+=(ord(i))
+index=index%10
+table[index]=value
+print(f'{name_1} is at index {index}: {table[index]}')
