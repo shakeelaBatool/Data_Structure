16b68b6921379310dd1268bdbf764a21573d82c5
Shakeela Batool <shakeelasadpara@gmail.com>
2026-02-24 20:01:18 +0500

    Update search_ele.py

--- search_ele.py
+++ search_ele.py
@@ -4,7 +4,7 @@ class Node:
         self.next=None    
 n1= Node(13)
 n1.next= Node(14)
-n1.next.next=Node(15)
+n1.next.next =Node(15)
 n1.next.next.next= Node(16)
 n1.next.next.next.next = Node(17)
 search=int(input("Enter your seach: "))
@@ -15,6 +15,7 @@ while temp != None:
         print("Find ")
         break   
     temp=temp.next
+
 
 
 
