bf7896bedc9c6f5e0fdeccc3baead750bc993ea8
Shakeela Batool <shakeelasadpara@gmail.com>
2026-02-24 20:00:47 +0500

    Update count_node.py

--- count_node.py
+++ count_node.py
@@ -7,7 +7,7 @@ class My_node:
 n1 = My_node(50)
 n2= My_node(40)
 n3 = My_node(30)
-node4 = My_node(20)
+node4  =My_node(20)
 
 n1.next = n2
 n2.next= n3
@@ -19,6 +19,7 @@ while temp !=None:
     count+= 1
     temp=temp.next
 print(f'Count is {count}')
+
 
 
 
