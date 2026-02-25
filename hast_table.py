0cecaa9616903af873dcb17fa1673f57e7d79064
Shakeela Batool <shakeelasadpara@gmail.com>
2026-02-25 20:12:02 +0500

    Update hast_table.py

--- hast_table.py
+++ hast_table.py
@@ -1,4 +1,4 @@
-table=[None]*10
+table= [None]*10
 print(len(table))
 name_1="Ali"
 value=25
@@ -7,4 +7,5 @@ for i in name_1:
     index+=(ord(i))
 index=index%10
 table[index]=value
+
 print(f'{name_1} is at index {index}: {table[index]}')
