d23b0493e9a2ea2381ad83e0550a482e3eb91c2b
Shakeela Batool <shakeelasadpara@gmail.com>
2026-02-27 11:00:48 +0500

    Fix indentation and formatting in stack_push_1.py

--- stack_puch_1.py
+++ stack_puch_1.py
@@ -2,7 +2,7 @@ stack=[]
 def push(x):
     stack.append(x)
 def pop():
-    if len(stack)== 0:
+    if len(stack) == 0:
         print("Stack is empty")
     else:
         print(stack.pop())
@@ -16,4 +16,5 @@ pop()
 pop()
 
 pop()
+
 
