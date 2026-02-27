611a70a20f094bce1160a901a596657089db2adb
Shakeela Batool <shakeelasadpara@gmail.com>
2026-02-27 11:00:07 +0500

    Update transversal+linked_list.py

--- transversal+linked_list.py
+++ transversal+linked_list.py
@@ -7,7 +7,7 @@ n1= Node(12)
 n2=Node(13)
 n3= Node(14)
 n4=Node(15)
-n1.next=n2
+n1.next= n2
 n2.next=n3
 n3.next =n4
 n2.prev= n1
@@ -24,6 +24,7 @@ while temp:
     temp=temp.prev
 
 print("None")
+
 
 
 
