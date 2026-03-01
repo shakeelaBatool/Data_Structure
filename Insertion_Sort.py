f6511d8a30ce958d023f53f4a4ff7727152202d3
Shakeela Batool <shakeelasadpara@gmail.com>
2026-03-01 12:55:57 +0500

    Fix formatting issues in Insertion_Sort.py

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -1,5 +1,5 @@
 arr= [7,12,8,9,3]
-n =len(arr)
+n = len(arr)
 #
 for i in range (n):
         for j in range(i+1,n):
@@ -8,4 +8,5 @@ for i in range (n):
                 arr[i],arr[j]=arr[j],arr[i]
             
 print(arr)
+
 
