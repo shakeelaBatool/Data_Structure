ec3da54f4d2c3a3765f8bb4a3abf681d315e5d16
Shakeela Batool <shakeelasadpara@gmail.com>
2026-03-12 09:36:23 +0500

    Remove duplicate code in hast_table.py

--- hast_table.py
+++ hast_table.py
@@ -1,6 +1,6 @@
 table= [None]*10
 print(len(table))
-name_1="Ali"
+name_1= "Ali"
 value=25
 index = 0
 for i in name_1:
@@ -9,6 +9,7 @@ index=index%10
 table[index]= value
 
 print(f'{name_1} is at index {index}: {table[index]}')
+
 
 
 
