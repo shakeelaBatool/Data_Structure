d2193ffb48070daebd68f02731f5c1925207dce0
Shakeela Batool <shakeelasadpara@gmail.com>
2026-03-15 08:32:30 +0500

    Fix formatting issues in Bubble_sort.py

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -4,7 +4,7 @@ n = len(list_bu)
 for i in range(n):
     for j in range(n - 1):   # sirf n-1 tak
         if list_bu[j] > list_bu[j + 1]:
-            list_bu[j], list_bu[j + 1] = list_bu[j + 1], list_bu[j]
+            list_bu[j], list_bu[j + 1] =list_bu[j + 1], list_bu[j]
 
 print(list_bu)
 
