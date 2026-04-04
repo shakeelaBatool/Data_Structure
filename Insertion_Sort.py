877a9b5e6bb8ab97e48efe8bcd76fca4da27c24e
Shakeela Batool <shakeelasadpara@gmail.com>
2026-04-04 22:18:57 +0500

    Update Insertion_Sort.py

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -4,7 +4,7 @@ n = len(arr)
 for i in range (n):
         for j in range(i+1,n):
             
-            if arr[i]> arr[j]:
+            if arr[i] > arr[j]:
                 arr[i],arr[j]=arr[j],arr[i]
             
 print(arr)
