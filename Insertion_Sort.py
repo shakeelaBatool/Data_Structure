5168f91c79241cafe67b200bbf2140276bee5548
Shakeela Batool <shakeelasadpara@gmail.com>
2026-05-30 17:47:36 +0500

    Refactor Insertion Sort implementation

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -5,7 +5,7 @@ for i in range (n):
         for j in range(i+1,n):
             
             if arr[i] > arr[j]:
-                arr[i],arr[j]=arr[j],arr[i]
+                arr[i],arr[j]= arr[j],arr[i]
             
 print(arr)
 
