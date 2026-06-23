42d860106216ff9b06239931f6209fed2a7be630
Shakeela Batool <shakeelasadpara@gmail.com>
2026-06-23 09:04:21 +0500

    Update element_inlinked_list.py

--- element_inlinked_list.py
+++ element_inlinked_list.py
@@ -3,7 +3,8 @@
 class Node:
     def __init__(self, data):
         self.data =data
-        self.next = None
+        self.next =
+        None
 n1= Node(12)
 n1.next= Node(13)
 n1.next.next= Node(14)
